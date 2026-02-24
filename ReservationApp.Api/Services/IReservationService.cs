7682295c83ef0a0d6dcf1e7a8f7c182537675cb7
Lester Rivera <les_rium@hotmail.com>
2026-02-24 15:44:50 -0600

    IReservationService updated for getbyid, update, delete

--- ReservationApp.Api/Services/IReservationService.cs
+++ ReservationApp.Api/Services/IReservationService.cs
@@ -3,6 +3,9 @@
     public interface IReservationService
     {
         Task<List<DTOs.ReservationDTO>> GetReservationsAsync();
+        Task<DTOs.ReservationDTO?> GetByIdAsync(int id);
         Task CreateReservationAsync(DTOs.CreateReservationDTO createReservation);
+        Task<bool> UpdateReservationAsync(int id, DTOs.CreateReservationDTO updateReservation);
+        Task<bool> DeleteReservationAsync(int id);
     }
 }
